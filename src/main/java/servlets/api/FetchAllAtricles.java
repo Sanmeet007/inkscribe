eed5997950dcb24ed0e1c7d6324fe7bcfb175168
Sanmeet007 <ssanmeet123@gmail.com>
2023-08-12 22:54:09 +0530

    created articles restapi

---
+++ src/main/java/servlets/api/FetchAllAtricles.java
@@ -0,0 +1,28 @@
+
+package servlets.api;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import utils.ResMethods;
+
+@WebServlet("/api/articles/get-articles")
+public class FetchAllAtricles extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) {
+
+        try {
+
+            if (req.getContentType().equals("application/json")) {
+            } else {
+            }
+        } catch (Exception e) {
+            ResMethods.writeJSONResponse(res, 500, ResMethods.get500ResJSON());
+        }
+
+        return;
+    }
+
+}
