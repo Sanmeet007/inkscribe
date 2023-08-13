b41a75472d2933f472f0d3df529916c79a5d19ff
Sanmeet007 <ssanmeet123@gmail.com>
2023-08-13 21:39:44 +0530

    fixed article page server redirect

--- src/main/java/servlets/ArticlePage.java
+++ src/main/java/servlets/ArticlePage.java
@@ -1,7 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,47 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.articles.Articles;
-
-@WebServlet("/article/*")
+@WebServlet("/article")
 public class ArticlePage extends HttpServlet {
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-
-        String path = request.getPathInfo();
-        String[] arr = path.split("/");
-        String slug = null;
-
-        ArrayList<String> processedArr = new ArrayList<String>();
-        for (String string : arr) {
-            if (string.equals("")) {
-                continue;
-            } else {
-                processedArr.add(string);
-            }
-        }
-
-        if (processedArr.size() == 1) {
-            slug = processedArr.get(0);
-        }
-
-        if (slug == null) {
-            response.sendError(404);
-            return;
-        } else {
-            try {
-                if (Articles.checkSlug(slug)) {
-                    request.setAttribute("slug", slug);
-                    request.getRequestDispatcher("/article.jsp").forward(request, response);
-                } else {
-                    response.sendError(404);
-                    return;
-                }
-            } catch (Exception e) {
-                response.sendError(500, "Something went wrong");
-                return;
-            }
-        }
+        response.sendRedirect("/");
     }
 }
