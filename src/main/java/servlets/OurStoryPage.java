0986e403298c78e820934a004da48599333438c8
Sanmeet007 <ssanmeet123@gmail.com>
2023-08-13 21:35:57 +0530

    bug fix

--- src/main/java/servlets/OurStoryPage.java
+++ src/main/java/servlets/OurStoryPage.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("")
+@WebServlet("/our-story")
 public class OurStoryPage extends HttpServlet {
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response)
