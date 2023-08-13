85a9a3bad21c877a3de66142836ae8bda4ba7fd9
Sanmeet007 <ssanmeet123@gmail.com>
2023-08-13 21:28:46 +0530

    fixed path

--- src/main/java/servlets/dashboard_pages/NewArticlePage.java
+++ src/main/java/servlets/dashboard_pages/NewArticlePage.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/dashboard/edit-article")
+@WebServlet("/dashboard/new-article")
 public class NewArticlePage extends HttpServlet {
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response)
